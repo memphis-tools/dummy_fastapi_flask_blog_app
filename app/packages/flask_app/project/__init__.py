e418f60d33f1adbc42d4b6d501cdbb59f6dd9480
memphis-tools <rn@github.com>
2024-04-12 13:17:36 +0200

    Revert "resolves-04-mise à jour fonctions add_book et login"
    This reverts commit cc2c6999b3e236c7ed99d696f42be85a23cfca6d.
    Commit fait dans branche development au lieu de passer par une branche
    dédiée

--- app/packages/flask_app/project/__init__.py
+++ app/packages/flask_app/project/__init__.py
@@ -263,7 +263,9 @@ def add_book():
         logs_context = {"current_user": f"{current_user.username}", "book_title": new_book.title}
         LOGGER.info("[+] Flask - Ajout livre", extra=logs_context)
         session.close()
-        return redirect(url_for("books"))
+        return render_template(
+            "index.html", is_authenticated=current_user.is_authenticated
+        )
     session.close()
     return render_template(
         "add_book.html", form=form, is_authenticated=current_user.is_authenticated
@@ -297,7 +299,7 @@ def login():
                 session.close()
                 return render_template(
                     "index.html",
-                    books=first_books,
+                    first_books=first_books,
                     is_authenticated=current_user.is_authenticated,
                 )
             else:
